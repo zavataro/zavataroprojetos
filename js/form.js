7d0bffb16e15a324e85b9c169924edc4b84667b7
zavataro <zavataro@gmail.com>
2023-10-19 22:21:02 -0300

    first commit

---
+++ js/form.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    $("form").submit(function (event) {
+      var formData = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        subject: $("#subject").val(),
+        body: $("#body").val(),
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      }).done(function (data) {
+        console.log(data);
+
+        if (!data.success) {
+            if (data.errors.name) {
+              $("#name-group").addClass("has-error");
+              $("#name-group").append(
+                '<div class="help-block">' + data.errors.name + "</div>"
+              );
+            }
+    
+            if (data.errors.email) {
+              $("#email-group").addClass("has-error");
+              $("#email-group").append(
+                '<div class="help-block">' + data.errors.email + "</div>"
+              );
+            }
+    
+            if (data.errors.subject) {
+              $("#superhero-group").addClass("has-error");
+              $("#superhero-group").append(
+                '<div class="help-block">' + data.errors.subject + "</div>"
+              );
+            }
+          } else {
+            $("form").html(
+              '<div class="alert alert-success">' + data.message + "</div>"
+            );
+          }
+
+
+      });
+  
+      event.preventDefault();
+    });
+  });
---
+++ process.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Digite seu nome.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Digite seu email.';
+}
+
+if (empty($_POST['subject'])) {
+    $errors['subject'] = 'Digite o assunto da mensagem.';
+}
+
+if (empty($_POST['body'])) {
+    $errors['body'] = 'Digite a mensagem que deseja enviar.';
+}
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
