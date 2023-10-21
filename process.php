59ac75639aadb07068591a77f733a9788f55a8e7
zavataro <zavataro@gmail.com>
2023-10-20 23:40:44 -0300

    implementando envio de email

--- process.php
+++ process.php
@@ -23,6 +23,20 @@ if (!empty($errors)) {
     $data['success'] = false;
     $data['errors'] = $errors;
 } else {
+
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "contato@zavataroprojetos.com";
+    $to = "gabriela@zavataroprojetos.com.br";
+    $subject = "Mensagem Enviada pelo Site";
+    $message = "Nome: " . $_POST['name'] <br /> .
+               "Email: " . $_POST['email'] <br /> .
+               "Assunto: " . $_POST['subject'] <br /> .
+               "Mensagem: " . $_POST['message'];
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
+
     $data['success'] = true;
     $data['message'] = 'Mensagem enviada com sucesso!';
 }
