6585230da8ddcb2c727bfbb9c76d7de8026a2a28
zavataro <zavataro@gmail.com>
2023-10-20 23:29:03 -0300

    ajustes em contato

--- js/form.js
+++ js/form.js
@@ -1,12 +1,29 @@
 ﻿$(document).ready(function () {
+    $("#name").on("keypress", function() {
+      $(".help-name-text").remove();
+    });
+
+    $("#email").on("keypress", function() {
+      $(".help-email-text").remove();
+    });
+
+    $("#subject").on("keypress", function() {
+      $(".help-subject-text").remove();
+    });
+
+    $("#message").on("keypress", function() {
+      $(".help-message-text").remove();
+    });
+
     $("form").submit(function (event) {
+      
       var formData = {
         name: $("#name").val(),
         email: $("#email").val(),
         subject: $("#subject").val(),
-        body: $("#body").val(),
+        message: $("#message").val(),
       };
-  
+
       $.ajax({
         type: "POST",
         url: "process.php",
@@ -14,33 +31,66 @@
         dataType: "json",
         encode: true,
       }).done(function (data) {
-        console.log(data);
-
         if (!data.success) {
             if (data.errors.name) {
-              $("#name-group").addClass("has-error");
-              $("#name-group").append(
-                '<div class="help-block">' + data.errors.name + "</div>"
+              $("#help-name").addClass("has-error");
+              $("#help-name").append(
+                '<div class="help-name-text">' + data.errors.name + "</div>"
               );
             }
     
             if (data.errors.email) {
-              $("#email-group").addClass("has-error");
-              $("#email-group").append(
-                '<div class="help-block">' + data.errors.email + "</div>"
+              $("#help-email").addClass("has-error");
+              $("#help-email").append(
+                '<div class="help-email-text">' + data.errors.email + "</div>"
               );
             }
     
             if (data.errors.subject) {
-              $("#superhero-group").addClass("has-error");
-              $("#superhero-group").append(
-                '<div class="help-block">' + data.errors.subject + "</div>"
+              $("#help-subject").addClass("has-error");
+              $("#help-subject").append(
+                '<div class="help-subject-text">' + data.errors.subject + "</div>"
               );
             }
+
+            if (data.errors.message) {
+              $("#help-message").addClass("has-error");
+              $("#help-message").append(
+                '<div class="help-message-text">' + data.errors.message + "</div>"
+              );
+            }
+
           } else {
-            $("form").html(
-              '<div class="alert alert-success">' + data.message + "</div>"
-            );
+              
+              var id = "#janela1";
+          
+              var alturaTela = $(document).height();
+              var larguraTela = $(window).width();
+            
+              //colocando o fundo preto
+              $('#mascara').css({'width':larguraTela,'height':alturaTela});
+              $('#mascara').fadeIn(1000); 
+              $('#mascara').fadeTo("slow",0.8);
+          
+              var left = ($(window).width() /2) - ( $(id).width() / 2 );
+              var top = ($(window).height() / 2) - ( $(id).height() / 2 );
+            
+              $(id).css({'top':top,'left':left});
+              $(id).show();   
+  
+              $("#mascara").click( function(){
+                  $(this).hide();
+                  $(".window").hide();
+              });
+              
+              $('.fechar').click(function(ev){
+                  ev.preventDefault();
+                  $("#mascara").hide();
+                  $(".window").hide();
+              });
+            //$("form").html(
+            //  '<div class="alert alert-success">' + data.message + "</div>"
+            //);
           }
 
 
--- process.php
+++ process.php
@@ -15,8 +15,8 @@ if (empty($_POST['subject'])) {
     $errors['subject'] = 'Digite o assunto da mensagem.';
 }
 
-if (empty($_POST['body'])) {
-    $errors['body'] = 'Digite a mensagem que deseja enviar.';
+if (empty($_POST['message'])) {
+    $errors['message'] = 'Digite a mensagem que deseja enviar.';
 }
 
 if (!empty($errors)) {
@@ -24,7 +24,7 @@ if (!empty($errors)) {
     $data['errors'] = $errors;
 } else {
     $data['success'] = true;
-    $data['message'] = 'Success!';
+    $data['message'] = 'Mensagem enviada com sucesso!';
 }
 
 echo json_encode($data);
